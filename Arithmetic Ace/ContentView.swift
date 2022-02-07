0b1b6c9522cd2db6ffd68b3df6c74a7118e8f975
Lillian Yang <lyang@lcs.on.ca>
2022-02-07 09:25:15 -0500

    Have part of the static interface for multiplication done.

--- Arithmetic Ace/ContentView.swift
+++ Arithmetic Ace/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack{
+            
+            HStack{
+            
+                Text("✕")
+                    
+                
+                Spacer()
+                
+                VStack{
+                    Text("5")
+                    Text("6")
+                }
+                
+                
+            }
+            
+            .font(.system(size: 72))
             .padding()
+            
+            Divider()
+            
+            Button(action: {
+                // NOTE: Output will not be shown unless this app is run in the "full" simulator
+                print("Button was pressed")
+            }, label: {
+                Text("Check Answer")
+            })
+            .buttonStyle(.bordered)
+
+        }
+       
+        
     }
 }
 
