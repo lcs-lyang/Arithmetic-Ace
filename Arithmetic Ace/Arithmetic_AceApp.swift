9bb1e3f48d7fd353e5647cf6f09dddfb97dbc98f
Lillian Yang <lyang@lcs.on.ca>
2022-02-07 08:52:02 -0500

    Initial Commit

---
+++ Arithmetic Ace/Arithmetic_AceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arithmetic_AceApp.swift
+//  Arithmetic Ace
+//
+//  Created by Lillian Yang on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct Arithmetic_AceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
